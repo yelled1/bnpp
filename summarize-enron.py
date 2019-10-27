ea6926e87306c02486912407e6b9eef1d452a710
hyoon <mailhyoon@gmail.com>
2019-10-27 00:59:44 -0400

    3rd modularization of functions

--- summarize-enron.py
+++ summarize-enron.py
@@ -13,20 +13,42 @@ import getopt
 import in_file_name from enron-event-history-all & grab in_column_names 
 to produce a sorted (by sent) csv file: person, sent, recieved 
 """
+def split_recipients(row, ):
+  "splits multiple recipients into pairs of list of [[sender & recipient], ...]"
+  return [[row.time, row.sender, recipient] for recipient in row.recipients.split('|')]
+  
 def read_input_csv(ifile):
   " read input_csv_file "
   in_column_names = ['time', 'msgId', 'sender', 'recipients'] #, 'topic', 'msg_mode']
   df = pd.read_csv(ifile, names=in_column_names, usecols=range(len(in_column_names)))
-  return df.fillna("")
-
-def split_recipients(row, ):
-  "splits multiple recipients into pairs of list of [[sender & recipient], ...]"
-  return [[row.time, row.sender, recipient] for recipient in row.recipients.split('|')]
-
+  # add column that contains list of recipients
+  df = df.fillna("")
+  df['send_rec_pair'] = df.apply(split_recipients, axis=1)
+  return df
+  
+def uniques_persons_set(df):
+  # find unique names by combining unique senders & recipients 
+  unique_senders = df.sender.unique().tolist()
+  unique_recipient = df.recipient.unique().tolist()
+  return set(unique_senders + unique_recipient)
+  
 def sender_recipient_counts(name, sender_count_d, recipient_count_d):
   """ returns: name, sender_#, recipient_# """
   return name, sender_count_d.get(name,0), recipient_count_d.get(name,0)
-
+  
+def combined_list_count(df, unique_people):
+  # use value_counts to get send & recipient count series to get 
+  # combined list_count
+  sender_num_dict = df.sender.value_counts().to_dict()
+  recipient_num_dict = df.recipient.value_counts().to_dict()
+  return [sender_recipient_counts(name, sender_num_dict, recipient_num_dict)
+                 for name in unique_people]
+  
+def sorted_out_df(list_counts):
+  """ return final sorted outfile for this project """
+  out_column_name = ['person', 'sent', 'received']
+  df = pd.DataFrame(list_counts, columns=out_column_name)
+  return df.sort_values(by='sent', ascending=False)
 
 def main(argv):
   #inputfile, outputfile = './enron-event-history-all.csv', './enron-email-count.csv'
@@ -48,35 +70,22 @@ def main(argv):
   print('Input file is "', inputfile)
   print('Output file is "', outputfile)
 
-
   # grabs input file & turn it into pandas df
   email_df = read_input_csv(inputfile)
-  # add column that contains list of recipients
-  email_df['send_rec_pair'] = email_df.apply(split_recipients, axis=1)
+  # merge the list into a flattened send_rec_pair in email_df
+  merged_df = pd.DataFrame(list(itertools.chain.from_iterable(email_df.send_rec_pair)),
+                           columns=['time', 'sender', 'recipient'])
 
-  # merge the list into a flattened df
-  merged_l = list(itertools.chain.from_iterable(email_df.send_rec_pair))
-  merged_df = pd.DataFrame(merged_l, columns=['time', 'sender', 'recipient'])
-
-  # find unique names by combining unique senders & recipients 
-  unique_senders = merged_df.sender.unique().tolist()
-  unique_recipient = merged_df.recipient.unique().tolist()
-  uniques_names = set(unique_senders + unique_recipient)
-
-  # use value_counts to get send & recipient count series to get 
-  # combined list_count
-  sender_num_dict = merged_df.sender.value_counts().to_dict()
-  recipient_num_dict = merged_df.recipient.value_counts().to_dict()
-  list_counts = [sender_recipient_counts(name,
-                                         sender_num_dict,
-                                         recipient_num_dict)
-                 for name in uniques_names]
+  # find unique persons & one can restrict people here
+  unique_persons = uniques_persons_set(merged_df) 
+  # time slices by restricting merged_df[merged_df.time <= utime]
+  list_counts = combined_list_count(merged_df, unique_persons)
 
   # convert to list to df, sort, & output to csv
-  out_column_name = ['person', 'sent', 'received']
-  out_df = pd.DataFrame(list_counts, columns=out_column_name)
-  out_df = out_df.sort_values(by='sent', ascending=False)
+  out_df = sorted_out_df(list_counts)
   out_df.to_csv(outputfile, index=False, sep='\t')
 
 if __name__ == "__main__":
+  print(time.ctime())
   main(sys.argv[1:])
+  print(time.ctime())
